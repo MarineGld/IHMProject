f25d342e9ee78e8a1f8a54d45d9c3c3c422091e1
Damien Arrivé <arrive.damien@gmail.com>
2022-05-02 18:33:35 +0200

    Damien fini

--- OffreStageController.java
+++ OffreStageController.java
@@ -24,150 +24,240 @@ import java.util.ResourceBundle;
 
 public class OffreStageController implements Initializable {
 
-        @FXML
-        private TableView<OffreStage> Table;
-        @FXML
-        private TableColumn<OffreStage, String> debutStage;
+    @FXML
+    private TableView<OffreStage> Table;
+    @FXML
+    private TableColumn<OffreStage, String> debutStage;
 
-        @FXML
-        private TableColumn<OffreStage, Integer> duree;
+    @FXML
+    private TableColumn<OffreStage, Integer> duree;
 
-        @FXML
-        private TableColumn<OffreStage, String> nomStructure;
+    @FXML
+    private TableColumn<OffreStage, String> nomStructure;
 
-        @FXML
-        private TableColumn<OffreStage, Integer> promo;
+    @FXML
+    private TableColumn<OffreStage, Integer> promo;
 
-        @FXML
-        private TableColumn<OffreStage, String> sujetStage;
+    @FXML
+    private TableColumn<OffreStage, String> sujetStage;
 
-        @FXML
-        private TextField Saisie_Nom;
+    @FXML
+    private TextField Saisie_Nom;
 
-        @FXML
-        private TextField Saisie_Sujet;
+    @FXML
+    private TextField Saisie_Sujet;
 
-        @FXML
-        private TextField Saisie_Duree;
+    @FXML
+    private TextField Saisie_Duree;
 
-        @FXML
-        private TextField Saisie_Date;
+    @FXML
+    private TextField Saisie_Date;
 
-        @FXML
-        private RadioButton Boutton_L3;
+    @FXML
+    private RadioButton Boutton_L3;
 
-        @FXML
-        private RadioButton Boutton_M1;
+    @FXML
+    private RadioButton Boutton_M1;
 
-        @FXML
-        private RadioButton Boutton_M2;
+    @FXML
+    private RadioButton Boutton_M2;
 
-        @FXML
-        private AnchorPane Carre_bleu;
+    @FXML
+    private AnchorPane Carre_bleu;
 
-        @FXML
-        private Button Boutton_Supprimer;
+    @FXML
+    private Button Boutton_Supprimer;
 
-        @FXML
-        private Button Boutton_Ajouter;
+    @FXML
+    private Button Boutton_Ajouter;
 
-        @FXML
-        private Button Boutton_Valider;
+    @FXML
+    private Button Boutton_Valider;
 
-        @FXML
-        private Button Boutton_Annuler;
+    @FXML
+    private Button Boutton_Annuler;
 
-        @FXML
-        ToggleGroup Promobutton;
-
-
-
-        public ObservableList<OffreStage> data = FXCollections.observableArrayList();
-        @FXML
-        public void viewStage(){
-            try (Connection connection = Database.connect(Database.location)){
-                String sql = "SELECT * FROM mesStages";
-                PreparedStatement stat = connection.prepareStatement(sql);
-                ResultSet rs = stat.executeQuery();
-                data.clear();
-                while(rs.next()) {
-                    data.add(new OffreStage(rs.getInt(1),rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getInt(6)));
-                }
-                connection.close();
+    @FXML
+    private ToggleGroup Promobutton;
 
 
-        } catch(Exception e){
-                e.printStackTrace();
+    public ObservableList<OffreStage> data = FXCollections.observableArrayList();
+
+    @FXML
+    public void viewStage() {
+        try (Connection connection = Database.connect(Database.location)) {
+            String sql = "SELECT * FROM mesStages;";
+            PreparedStatement stat = connection.prepareStatement(sql);
+            ResultSet rs = stat.executeQuery();
+            data.clear();
+            while (rs.next()) {
+                data.add(new OffreStage(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getInt(6)));
             }
+            connection.close();
 
-                nomStructure.setCellValueFactory(new PropertyValueFactory<OffreStage, String>("nomStructure"));
-                sujetStage.setCellValueFactory(new PropertyValueFactory<OffreStage, String>("sujetStage"));
-                debutStage.setCellValueFactory(new PropertyValueFactory<OffreStage, String>("debutStage"));
-                duree.setCellValueFactory(new PropertyValueFactory<OffreStage, Integer>("duree"));
-                promo.setCellValueFactory(new PropertyValueFactory<OffreStage, Integer>("promo"));
-                Table.setItems(data);
-            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        nomStructure.setCellValueFactory(new PropertyValueFactory<OffreStage, String>("nomStructure"));
+        sujetStage.setCellValueFactory(new PropertyValueFactory<OffreStage, String>("sujetStage"));
+        debutStage.setCellValueFactory(new PropertyValueFactory<OffreStage, String>("debutStage"));
+        duree.setCellValueFactory(new PropertyValueFactory<OffreStage, Integer>("duree"));
+        promo.setCellValueFactory(new PropertyValueFactory<OffreStage, Integer>("promo"));
+        Table.setItems(data);
+    }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
-        private Accueil accueil;
+
+    private Accueil accueil;
+
     public OffreStageController(Accueil accueil) {
-            this.accueil = accueil;
+        this.accueil = accueil;
+    }
+
+    @FXML
+    public void goToAccueil(ActionEvent e) throws IOException {
+        Button button = (Button) e.getSource();
+        FXMLLoader newScene = new FXMLLoader(getClass().getResource("Accueil.fxml"));
+        newScene.setController(accueil.getAccueilController());
+        Stage leStage = (Stage) button.getScene().getWindow();
+        leStage.setScene(new Scene(newScene.load()));
+    }
+
+    @FXML
+    public void actAjouter(ActionEvent e) {
+        Button button = (Button) e.getSource();
+        Carre_bleu.setVisible(true);
+        Saisie_Nom.setText("");
+        Saisie_Sujet.setText("");
+        Saisie_Duree.setText("");
+        Saisie_Date.setText("");
+        Boutton_L3.setSelected(false);
+        Boutton_M1.setSelected(false);
+        Boutton_M2.setSelected(false);
+        Boutton_Supprimer.setVisible(false);
+
+    }
+
+    @FXML
+    public void actAnnuler(ActionEvent e) {
+        Button button = (Button) e.getSource();
+        Carre_bleu.setVisible(false);
+        /*  Boutton_Ajouter.setVisible(true); */
+    }
+
+    @FXML
+    public void actValider(ActionEvent e) {
+        Button button = (Button) e.getSource();
+        Carre_bleu.setVisible(false);
+        if (Boutton_Supprimer.isVisible()) { addModifyStage(2); }
+        else { addModifyStage(1); }
+        viewStage();
+        /* Boutton_Ajouter.setVisible(true); */
+    }
+
+    @FXML
+    public void actSupprimer(ActionEvent e) {
+        Button button = (Button) e.getSource();
+        Carre_bleu.setVisible(false);
+        deleteStage();
+        viewStage();
+        /*Boutton_Ajouter.setVisible(true);*/
+    }
+
+    @FXML
+    public void modifSetUp() {
+        if (Table.getSelectionModel().getSelectedItem() != null) {
+            OffreStage lestage = (OffreStage) Table.getSelectionModel().getSelectedItem();
+
+            Carre_bleu.setVisible(true);
+            Saisie_Nom.setText(lestage.getNomStructure());
+            Saisie_Sujet.setText(lestage.getSujetStage());
+            Saisie_Duree.setText(String.valueOf(lestage.getDuree()));
+            Saisie_Date.setText(lestage.getDebutStage());
+            if (lestage.getPromo() == "L3") {
+                Boutton_L3.setSelected(true);
+            } else if (lestage.getPromo() == "M1") {
+                Boutton_M1.setSelected(true);
+            } else if (lestage.getPromo() == "M2") {
+                Boutton_M2.setSelected(true);
+            }
+            Boutton_Supprimer.setVisible(true);
+            /*Boutton_Ajouter.setVisible(false);*/
         }
 
-        public void goToAccueil (ActionEvent e) throws IOException {
-            Button button = (Button) e.getSource();
-            FXMLLoader newScene = new FXMLLoader(getClass().getResource("Accueil.fxml"));
-            newScene.setController(accueil.getAccueilController());
-            Stage leStage = (Stage) button.getScene().getWindow();
-            leStage.setScene(new Scene(newScene.load()));
+    }
+
+    @FXML
+    public void deleteStage() {
+        try (Connection connection = Database.connect(Database.location)) {
+            OffreStage lestage = (OffreStage) Table.getSelectionModel().getSelectedItem();
+            String idStage = String.valueOf(lestage.getId());
+            String sql = "DELETE FROM mesStages WHERE ID = ";
+            PreparedStatement stat = connection.prepareStatement(sql+idStage);
+            stat.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
-        public void addStage (ActionEvent e) {
-          Button button = (Button) e.getSource();
-            OffreStage lestage = data.get(1);
-          Carre_bleu.setVisible(true);
-
     }
 
-        public void actAnnuler (ActionEvent e) {
-            Button button = (Button) e.getSource();
-            Carre_bleu.setVisible(false);
-        //    Boutton_Ajouter.setVisible(true);
+    public boolean isNum(String name) {
+        char[] chars = name.toCharArray();
+        for (char c : chars) {
+            if(Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 
-        public void actValider (ActionEvent e) {
-            Button button = (Button) e.getSource();
+    @FXML
+    public void addModifyStage(int choix) {
+        try (Connection connection = Database.connect(Database.location)) {
+            boolean valide = true;
 
-            Carre_bleu.setVisible(false);
-            Boutton_Ajouter.setVisible(true);
-    }
+            String leNom = Saisie_Nom.getText();
+            if (leNom=="") { valide = false;}
 
-        public void modif () {
-            if (Table.getSelectionModel().getSelectedItem() != null) {
-                OffreStage lestage = (OffreStage) Table.getSelectionModel().getSelectedItem();
+            String leSujet = Saisie_Sujet.getText();
+            if (leSujet=="") { valide = false;}
 
-                Carre_bleu.setVisible(true);
-                Saisie_Nom.setText(lestage.getNomStructure());
-                Saisie_Sujet.setText(lestage.getSujetStage());
-                Saisie_Duree.setText(String.valueOf(lestage.getDuree()));
-                Saisie_Date.setText(lestage.getDebutStage());
-                if (lestage.getPromo() == "L3") {
-                    Boutton_L3.setSelected(true);
+            String laDuree = "";
+            if (isNum(Saisie_Duree.getText())) {
+                laDuree = Saisie_Duree.getText();
+            } else { valide = false;}
+
+            String laDate = Saisie_Date.getText();
+            if (leSujet=="") { valide = false;}
+
+            String laPromo = "0";
+            if (Boutton_L3.isSelected()) {
+                laPromo = "1";
+            } else if (Boutton_M1.isSelected()) {
+                laPromo = "2";
+            } else if (Boutton_M2.isSelected()){
+                laPromo = "3";
+            } else { valide = false;}
+
+            if (valide) {
+                String sql = "";
+                if (choix==1) { sql = "INSERT INTO mesStages VALUES (null, '" + leNom + "', '" + leSujet + "', " + laDuree + ", '" + laDate + "', " + laPromo + ")"; }
+                else {
+                    OffreStage lestage = (OffreStage) Table.getSelectionModel().getSelectedItem();
+                    String idStage = String.valueOf(lestage.getId());
+                    sql = "UPDATE mesStages SET nomStructure = '" + leNom + "', sujetStage = '" + leSujet + "', duree =" + laDuree + ", debutStage = '" + laDate + "', promo = " + laPromo + " where id = " + idStage;
                 }
-                else if (lestage.getPromo() == "M1") {
-                    Boutton_M1.setSelected(true);
-                }
-                else if (lestage.getPromo() == "M2") {
-                    Boutton_M2.setSelected(true);
-                }
-                Boutton_Supprimer.setVisible(true);
+                PreparedStatement stat = connection.prepareStatement(sql);
+                stat.executeUpdate();
             }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    }
+}
